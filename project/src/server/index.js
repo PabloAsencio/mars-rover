58c7167f075a1b718aaf8553af335074a2b6cc5b
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2021-07-14 17:57:50 +0200

    feat: Create functions to fetch results from the NASA API

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -13,19 +13,6 @@ app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, '../public')));
 
 // your API calls
-
-// example API call
-app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(
-            `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`
-        ).then((res) => res.json());
-        res.send({ image });
-    } catch (err) {
-        console.log('error:', err);
-    }
-});
-
 app.get('/rover/:roverName', async (req, res) => {
     try {
         fetch(
@@ -38,4 +25,79 @@ app.get('/rover/:roverName', async (req, res) => {
     }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const makePhotoObject = (photo) => {
+    const { camera, img_src, rover, earth_date } = photo;
+    return {
+        img_src,
+        earth_date,
+        camera: camera.full_name,
+        rover: rover.name,
+    };
+};
+
+const getLastThreePictures = async (rover, max_sol) => {
+    const result = [];
+    try {
+        const response = await fetch(
+            `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.API_KEY}`
+        );
+        const responseJSON = await response.json();
+        const latestPhotos = responseJSON.latest_photos.map(makePhotoObject);
+        const missingPhotos =
+            3 -
+            result.push(...latestPhotos.filter((current, index) => index < 3));
+        if (missingPhotos > 0) {
+            const secondResponse = await fetch(
+                `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${
+                    max_sol - 1
+                }&api_key=${process.env.API_KEY}`
+            );
+            const secondResponseJSON = await secondResponse.json();
+            const additionalPhotos = secondResponseJSON.photos.map(
+                makePhotoObject
+            );
+            result.push(
+                additionalPhotos.filter(
+                    (current, index) => index < missingPhotos
+                )
+            );
+        }
+    } catch (error) {
+        console.log(error);
+        throw new Error(error.message);
+    }
+    return result;
+};
+
+app.get('/manifests/:roverName', async (req, res) => {
+    try {
+        fetch(
+            `https://api.nasa.gov/mars-photos/api/v1/manifests/${req.params.roverName}?api_key=${process.env.API_KEY}`
+        )
+            .then((response) => response.json())
+            .then(async (roverInfo) => {
+                const {
+                    name,
+                    launch_date,
+                    landing_date,
+                    status,
+                    max_date,
+                    max_sol,
+                } = roverInfo.photo_manifest;
+                const photos = await getLastThreePictures(name, max_sol);
+                return {
+                    name,
+                    launch_date,
+                    landing_date,
+                    status,
+                    max_date,
+                    photos,
+                };
+            })
+            .then((data) => res.send(data));
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+app.listen(port, () => console.log(`Mars rovers app listening on port ${port}!`));
